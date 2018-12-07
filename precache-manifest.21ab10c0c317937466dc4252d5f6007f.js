21e4220b92223d213793060efc1b6563bd629c2b
Mladen Dimitrov <bazik_2002@yahoo.com>
2018-12-07 22:07:44 +0200

    Updates

---
+++ precache-manifest.21ab10c0c317937466dc4252d5f6007f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "4d3072adc2d1504adcac",
+    "url": "/contactmanager/static/js/main.4d3072ad.chunk.js"
+  },
+  {
+    "revision": "326312229a65259c2c30",
+    "url": "/contactmanager/static/js/1.32631222.chunk.js"
+  },
+  {
+    "revision": "4d3072adc2d1504adcac",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "326312229a65259c2c30",
+    "url": "/contactmanager/static/css/1.0b52e271.chunk.css"
+  },
+  {
+    "revision": "b40937743da4003e6e1709617c043f99",
+    "url": "/contactmanager/index.html"
+  }
+];
